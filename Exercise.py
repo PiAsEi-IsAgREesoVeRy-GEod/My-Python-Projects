3a4312c6b3c6717fffeff105796c246627c3744e
Pisei <pisei@api-school.com>
2025-07-09 08:52:22 +0700

    Create Exercise.py

---
+++ Exercise.py
@@ -0,0 +1,9 @@
+question = input("Do you want to exercise? ")
+if question == "yes":
+    print("Good, let's start!")
+else:
+    question = input("Are you sure? ")
+if question == "yes":
+    print("Fine, but don't complain about being unhealthy to your therapist! ")
+else:
+    print("Let's start!")
